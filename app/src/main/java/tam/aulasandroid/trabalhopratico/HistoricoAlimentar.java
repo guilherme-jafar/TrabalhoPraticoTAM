223e7cc94ef4caa4501f668ad396135f63947a73
guilherme-jafar <gjafar14@gmail.com>
2021-05-31 12:04:55 +0100

    Histórico alimantar

---
+++ app/src/main/java/tam/aulasandroid/trabalhopratico/HistoricoAlimentar.java
@@ -0,0 +1,14 @@
+package tam.aulasandroid.trabalhopratico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoricoAlimentar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico_alimentar);
+    }
+}
